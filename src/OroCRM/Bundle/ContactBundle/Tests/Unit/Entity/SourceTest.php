4b480ff5aebc05f956657933accd8feb483b2313
Natalya Moshlyak <NatalyaMoshlyak@users.noreply.github.com>
2014-04-03 21:25:37 -0700

    Update SourceTest.php
    Memory is allocated for each new string unless it is defined once as a constant. It also allows for quick change of the value of the string and ensures that we are testing for the same string in unit tests.

--- src/OroCRM/Bundle/ContactBundle/Tests/Unit/Entity/SourceTest.php
+++ src/OroCRM/Bundle/ContactBundle/Tests/Unit/Entity/SourceTest.php
@@ -6,17 +6,20 @@ use OroCRM\Bundle\ContactBundle\Entity\Source;
 
 class SourceTest extends \PHPUnit_Framework_TestCase
 {
+    define("TEST", "test");
+    define("TEST_LABEL", "TEST_LABEL");
+
     public function testName()
     {
-        $obj = new Source('test');
-        $this->assertEquals('test', $obj->getName());
+        $obj = new Source(TEST);
+        $this->assertEquals(TEST, $obj->getName());
     }
 
     public function testLabel()
     {
-        $obj = new Source('test');
-        $obj->setLabel('TEST_LABEL');
-        $this->assertEquals('TEST_LABEL', $obj->getLabel());
-        $this->assertEquals('TEST_LABEL', $obj->__toString());
+        $obj = new Source(TEST);
+        $obj->setLabel(TEST_LABEL);
+        $this->assertEquals(TEST_LABEL, $obj->getLabel());
+        $this->assertEquals(TEST_LABEL, $obj->__toString());
     }
 }
